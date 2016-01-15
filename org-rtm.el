9830d4ea3850142bab704ba4980a9b5b922ad331
Philipp Middendorf <pmidden@secure.mailbox.org>
2016-01-15 15:19:14 +0100

    add license and packaging information

--- org-rtm.el
+++ org-rtm.el
@@ -1,5 +1,16 @@
+;;; org-rtm.el --- Primitive import/export plugin for rememberthemilk tasks to org-mode
+;; Copyright (C) 2016 Philipp Middendorf
+;; Author: Philipp Middendorf <pmidden@secure.mailbox.org>
+;; Created: 15 Jan 2016
+;; Keywords: outlines, data
+;; Homepage: https://github.com/pmiddend/org-rtm
+;; This file is not part of GNU Emacs.
+;;; Commentary:
+;; The project is hosted at htts://github.com/pmiddend/org-rtm
+;; The latest version, and all the relevant information can be found there.
 (require 'rtm)
 
+;;; Code:
 (defgroup org-rtm ()
   "Retrieve and complete tasks from rememberthemilk.com and convert them to org-mode"
   :group 'external
@@ -7,7 +18,7 @@
   :prefix "org-rtm-")
 
 (defcustom org-rtm-import-file "~/rtm.org"
-  "Where to export the contents of RTM to when using org-rtm-import"
+  "Where to export the contents of RTM to when using org-rtm-import."
   :group 'org-rtm
   :type 'file)
 
@@ -19,32 +30,33 @@ the import process is working well."
   :type 'boolean)
 
 (defun org-rtm-assoc-value (symbol list)
+  "Get the value behind SYMBOL in an association LIST (not the pair of key/value)."
   (cdr (assoc symbol list)))
 
 (defun org-rtm-print-list (rtml)
-  "Convert an RTM list to an org mode segment (top level, starting with *)"
+  "Convert an RTM list RTML to an org mode segment (top level, starting with *)."
   (progn
     (concat
      "* "
      (org-rtm-assoc-value 'name (car (cdr rtml))))))
 
 (defun org-rtm-format-note (note)
-  "Format a single RTM task note"
+  "Format a single RTM task NOTE."
   (nth 2 note))
 
 (defun org-rtm-format-notes (notes-list)
-  "Format a list of RTM task notes and concatenate to string"
+  "Format a list NOTES-LIST of RTM task notes and concatenate to string."
   (cond ((equal (length notes-list) 0) "")
 	(t (concat "\n" (mapconcat 'org-rtm-format-note notes-list "\n")))))
 
 (defun org-rtm-format-time-to-org (time-value)
-  "Convert an ISO date time to the org-mode time format. 
+  "Convert an ISO date time TIME-VALUE to the org-mode time format. 
 I didn't find built-in function to accomplish this."
   (print (length time-value))
   (format-time-string (cdr org-time-stamp-formats) (date-to-time time-value)))
 
 (defun org-rtm-print-entry (e)
-  "Format a single RTM task and output as second level org segment (starting with **)"
+  "Format a single RTM task E and output as second level org segment (starting with **)."
   (let*
       ((topAssocList (cdr e))
        (taskAssocList (car topAssocList))
@@ -59,41 +71,42 @@ I didn't find built-in function to accomplish this."
        (org-rtm-format-notes notes-list)))))
 
 (defun org-rtm-format-list-entries (list-id)
-  "Convert a single RTM task list to org mode items"
+  "Convert a single RTM task list LIST-ID to org mode items."
   (mapconcat 'org-rtm-print-entry (nthcdr 2 (car (rtm-tasks-get-list list-id "status:incomplete"))) "\n"))
 
 (defun org-rtm-format-list (list-id list-name)
-  "Format a single list as a top-level org-mode segment starting with *"
+  "Format a single list with LIST-ID and LIST-NAME as a top-level org-mode segment starting with *."
   (progn
     (concat "* " list-name "\n" (org-rtm-format-list-entries list-id))))
 
 (defun org-rtm-format-lists ()
-  "Format RTM lists to org mode segments"
+  "Format RTM lists to org mode segments."
   (mapconcat (lambda (list) (org-rtm-format-list (org-rtm-assoc-value 'id (nth 1 list)) (org-rtm-assoc-value 'name (nth 1 list)))) (rtm-lists-get-list) "\n"))
 
 (defun org-rtm-complete-items (list-id)
-  "Complete all items in a given RTM list (doesn't do any org-mode conersion')"
+  "Complete all items in a given RTM list with LIST-ID (doesn't do any org-mode conversion)."
   (mapcar
    (lambda (taskseries-w-task)
      (rtm-tasks-complete list-id (car taskseries-w-task) (cdr taskseries-w-task)))
    (org-rtm-retrieve-taskseries-id-with-task-list list-id)))
 
 (defun org-rtm-retrieve-taskseries-id-with-task-list (list-id)
+  "Retrieves the taskseries entry for list with LIST-ID."
   (mapcar
    (lambda (list)
      `(,(org-rtm-assoc-value 'id (nth 1 list)) . ,(org-rtm-assoc-value 'id (car (org-rtm-assoc-value 'task (nthcdr 2 list))))))
    (nthcdr 2 (car (rtm-tasks-get-list list-id "status:incomplete")))))
 
 (defun org-rtm-retrieve-list-ids ()
-  "Retrieve a list of RTM list ids"
+  "Retrieve a list of RTM list ids."
   (mapcar (lambda (list) (org-rtm-assoc-value 'id (nth 1 list))) (rtm-lists-get-list)))
 
 (defun org-rtm-complete-all-lists ()
-  "Complete all items of RTM all lists"
+  "Complete all items of RTM all lists."
   (mapcar (lambda (list-id) (org-rtm-complete-items list-id)) (org-rtm-retrieve-list-ids)))
 
 (defun org-rtm-import ()
-  "Import RTM tasks to the given import file (overwriting it), then optionally completing the tasks, then opening the file in emacs"
+  "Import RTM tasks to the given import file (overwriting it), then optionally completing the tasks, then opening the file in Emacs."
   (interactive)
   (message "Starting RTM import...")
   (let
@@ -109,3 +122,4 @@ I didn't find built-in function to accomplish this."
     (insert import-data)))
 
 (provide 'org-rtm)
+;;; org-rtm.el ends here
