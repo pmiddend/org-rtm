13a628fb81dfb06e0d2cc7f71923bab275983890
Philipp Middendorf <pmidden@secure.mailbox.org>
2016-01-15 15:39:43 +0100

    fix dependency list in -pkg.el

--- org-rtm-pkg.el
+++ org-rtm-pkg.el
@@ -1,1 +1,5 @@
-(define-package org-rtm "0.1" "Primitive import/export plugin for rememberthemilk tasks to org-mode" ((org "7")))
+(define-package
+  org-rtm
+  "0.1"
+  "Primitive import/export plugin for rememberthemilk tasks to org-mode"
+  '((org "7")))
