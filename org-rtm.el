b40475213d32ea46d0bbb055c3d682242140794c
Philipp Middendorf <pmidden@secure.mailbox.org>
2016-01-19 15:24:29 +0100

    another readme change (github url wrong)

--- org-rtm.el
+++ org-rtm.el
@@ -18,7 +18,7 @@
 ;;; Commentary:
 ;; Simple import/export from rememberthemilk to org-mode
 ;;
-;; The project is hosted at htts://github.com/pmiddend/org-rtm
+;; The project is hosted at https://github.com/pmiddend/org-rtm
 ;; The latest version, and all the relevant information can be found there.
 ;;; Code:
 (require 'rtm)
